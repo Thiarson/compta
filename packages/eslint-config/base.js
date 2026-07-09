e5192fba6a330d35da279da56a8e247338676d39
Thiarson Antsa <thiarsonantsa@gmail.com>
2026-07-09 08:25:10 +0300

    config: update elsint and prettier configuration

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  prettier,
+]);
